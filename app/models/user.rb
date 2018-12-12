fb701139bf8de28b1e05a0423c871107aca58d18
Brennan Blair <bblair31@users.noreply.github.com>
2018-12-12 17:24:17 -0500

    built models and tested associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :transactions
+  has_many :stocks, through: :transactions
+  has_many :searches
   validates :username, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: { case_sensitive: false }
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :username, :email
+  has_many :transactions
+  has_many :stocks, through: :transactions
+  has_many :searches
 end
