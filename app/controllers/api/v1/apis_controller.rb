a6d806869430fb954b7ac34b6031701e2fea41c6
Brennan Blair <bblair31@users.noreply.github.com>
2018-12-12 13:09:36 -0500

    Add initial custom apis_controller actions and corresponding routes

--- app/controllers/api/v1/apis_controller.rb
+++ app/controllers/api/v1/apis_controller.rb
@@ -1,9 +1,43 @@
 class Api::V1::ApisController < ApplicationController
 
   def markets
-    @data = RestClient.get 'https://api.iextrading.com/1.0/market'
-
-    render json: @data
+    @markets = RestClient.get 'https://api.iextrading.com/1.0/market'
+    render json: @markets
   end
 
-end
+  def most_active
+    @most_active = RestClient.get 'https://api.iextrading.com/1.0/stock/market/list/mostactive'
+    render json: @most_active
+  end
+
+  def gainers
+    @gainers = RestClient.get 'https://api.iextrading.com/1.0/stock/market/list/gainers'
+    render json: @gainers
+  end
+
+  def losers
+    @losers = RestClient.get 'https://api.iextrading.com/1.0/stock/market/list/losers'
+    render json: @losers
+  end
+
+  def market_news
+    @market_news = RestClient.get 'https://api.iextrading.com/1.0/stock/market/news/last/50'
+    render json: @market_news
+  end
+
+  def sector_performance
+    @sector_performance = RestClient.get 'https://api.iextrading.com/1.0/stock/market/sector-performance'
+    render json: @sector_performance
+  end
+
+  def in_focus
+    @in_focus = RestClient.get 'https://api.iextrading.com/1.0/stock/market/list/infocus'
+    render json: @in_focus
+  end
+
+  def earnings_today
+    @earnings_today = RestClient.get 'https://api.iextrading.com/1.0/stock/market/today-earnings'
+    render json: @earnings_today
+  end
+
+end ### End of API Controller
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
    namespace :v1 do
      get '/markets', to: 'apis#markets'
+     get '/mostactive', to: 'apis#most_active'
+     get '/gainers', to: 'apis#gainers'
+     get '/losers', to: 'apis#losers'
+     get '/marketnews', to: 'apis#market_news'
+     get '/sectorperformance', to: 'apis#sector_performance'
+     get '/infocus', to: 'apis#in_focus'
+     get '/earningstoday', to: 'apis#earnings_today'
    end
  end
 end
