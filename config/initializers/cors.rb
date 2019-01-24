46b9f41a40a2db1c87a270d4f9c570da1335ec17
Brennan Blair <bblair31@users.noreply.github.com>
2019-01-24 15:28:20 -0500

    Change CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['market-viz.herokuapp.com/']
+    origins ['market-viz.herokuapp.com']
 
     resource '*',
       headers: :any,
